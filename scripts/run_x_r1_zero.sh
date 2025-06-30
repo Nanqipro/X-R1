3b5aefcfc82af481e0f690c12e5f9caa27fa56a5
Nanqipro <2280285631@qq.com>
2025-06-30 21:28:10 +0800

    72

--- scripts/run_x_r1_zero.sh
+++ scripts/run_x_r1_zero.sh
@@ -20,3 +20,12 @@ ACCELERATE_LOG_LEVEL=info accelerate launch \
 --num_processes=3 src/x_r1/grpo.py \
 --config recipes/X_R1_zero_3B_config.yaml \
 > ./output/x_r1_3B_sampling.log 2>&1
+
+# 禁用 DeepSpeed 的 CUDA 扩展编译，通过设置环境变量让 DeepSpeed 使用 CPU 回退模式
+CUDA_VISIBLE_DEVICES=0,1,2 DS_BUILD_OPS=0 DS_SKIP_CUDA_CHECK=1 \
+ACCELERATE_LOG_LEVEL=info accelerate launch \
+--config_file recipes/zero3.yaml \
+--num_processes=3 src/x_r1/grpo.py \
+--config recipes/X_R1_zero_3B_config.yaml \
+> ./output/x_r1_3B_sampling.log 2>&1
+
